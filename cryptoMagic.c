688539a6e201aec2055f5e479d6034a2bb05c81d
JJBH26 <146396485+JJBH26@users.noreply.github.com>
2025-01-17 15:43:41 -0500

    Add files via upload
    This is my second command in this command I finished my work doing the 3 first points of the assignement
    I created a function to show how to use the code and I worked on the main function in which the code takes the arguments given by the user determines how many they are and then it depending on what the user gives it, it decrypts or Encrypts the file (the code for the decrypting and encrypting still needs to be done)

---
+++ cryptoMagic.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+    if (argc < 2) {
+        printUsage();
+        return 1;
+    }
+
+
+    int mode = 1;    // here I created a Flag so If the user doesnt enter 
+                            // neither encrypt or decrypt its set to encrypting
+    char *fileName = NULL;
+
+    if (argc == 2){
+        filename = argv[1];
+    }else if (argc == 3){
+        if (strcmp(argv[1], "--encrypt")== 0){
+            mode = 1; //This line doesnt really do anything its just there to clarify
+        }else if (strcmp(argv[1],  "--decrypt")== 0){
+            mode = 0; // This just puts it into the decrypting mode
+        }else {
+            printf("Error Invalid entry '%s'",argv[1]);
+            printfUsage();
+            return 1;
+        }
+        fileName = argv[2];
+    }
+
+    //Im just putting this for now but you guys have to add the logic
+    if(mode == 1){
+        //here is where teh encrypting code goes
+    }else (mode == 0){
+        //here is where the decrypting code goes
+    }
+    return 0;
+}
+
+//This is just a function that I created so the user can see how he has to use the code
+//its kind of a help command.
+void printfUsage(){
+    printf("How to use it: cryptoMagic [--encrypt or --decrypt] <filename>\n");
+    printf("  --encrypt   Encrypt the input file (this would also be the default)\n");
+    printf("  --decrypt   Decrypt the input file\n");
+}
