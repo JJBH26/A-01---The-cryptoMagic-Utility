fba95d5300fb6082515abcc8fcc2ed07bf6c17c3
Lukas Fukuoka <135397104+comet400@users.noreply.github.com>
2025-01-17 19:12:41 -0500

    Update cryptoMagic.c
    fixed typos and added the prototypes

--- cryptoMagic.c
+++ cryptoMagic.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+//Insert Our prototypes here
+void printfUsage();
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]) {
     if (argc < 2) {
-        printUsage();
+        printfUsage();
         return 1;
     }
 
 
     int mode = 1;    // here I created a Flag so If the user doesnt enter 
-                            // neither encrypt or decrypt its set to encrypting
-    char *fileName = NULL;
+    // neither encrypt or decrypt its set to encrypting
+    char* fileName = NULL;
 
-    if (argc == 2){
-        filename = argv[1];
-    }else if (argc == 3){
-        if (strcmp(argv[1], "--encrypt")== 0){
+    if (argc == 2) {
+        fileName = argv[1];
+    }
+    else if (argc == 3) {
+        if (strcmp(argv[1], "--encrypt") == 0) {
             mode = 1; //This line doesnt really do anything its just there to clarify
-        }else if (strcmp(argv[1],  "--decrypt")== 0){
+        }
+        else if (strcmp(argv[1], "--decrypt") == 0) {
             mode = 0; // This just puts it into the decrypting mode
-        }else {
-            printf("Error Invalid entry '%s'",argv[1]);
+        }
+        else {
+            printf("Error Invalid entry '%s'", argv[1]);
             printfUsage();
             return 1;
         }
@@ -29,9 +34,10 @@ int main(int argc, char *argv[]){
     }
 
     //Im just putting this for now but you guys have to add the logic
-    if(mode == 1){
+    if (mode == 1) {
         //here is where teh encrypting code goes
-    }else (mode == 0){
+    }
+    else (mode == 0) {
         //here is where the decrypting code goes
     }
     return 0;
@@ -39,7 +45,7 @@ int main(int argc, char *argv[]){
 
 //This is just a function that I created so the user can see how he has to use the code
 //its kind of a help command.
-void printfUsage(){
+void printfUsage() {
     printf("How to use it: cryptoMagic [--encrypt or --decrypt] <filename>\n");
     printf("  --encrypt   Encrypt the input file (this would also be the default)\n");
     printf("  --decrypt   Decrypt the input file\n");
